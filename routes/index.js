4eb16c6b4008c561cd7372b4a286e704a9e634a3
jim <jim@VM-Dev>
2015-06-04 22:01:16 -0500

    basic header and initial polymer scripts set

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ChitChat'})
+  /*res.render('index', { title: 'ChitChat'})*/
+	res.sendFile('index.html',{root: path.resolve(__dirname,'..','html')});
 });
 
 module.exports = router;
