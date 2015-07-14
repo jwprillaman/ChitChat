a6dbcb995243024702779058fc5b48f94f874eca
jim <jim@AthenasGhost>
2015-07-14 15:20:09 -0500

    username now sent when message to server

--- js/controllers.js
+++ js/controllers.js
@@ -6,8 +6,16 @@ ang.controller('HomeController',function($scope,User){
 	$scope.User = User;
 	$scope.banner = "Welcome";
 });
-ang.controller('WalkieController',function($scope){
+ang.controller('WalkieController',function($scope,User){
 	$scope.banner = "Walkie";
+	$scope.User = User;
+
+	$scope.sendMessage = function(){
+	  var data = { user: User.name,
+	               message: $('#message').val()}
+	  socket.emit('message', data);
+	  $('#message').val('');
+	};
 });
 ang.controller('SettingsController',function($scope){
 	$scope.banner = "Settings";
--- js/messaging.js
+++ js/messaging.js
@@ -1,10 +1,7 @@
+
+var socket = io();
+
 socket.on('message', function(data){
 	$('#walkie-view-chat').append($('<li>').text(data.user + ': ' + data.message));
 });
 
-function sendMessage(){
-	var data = { user: 'anon',
-							 message: $('#message').val()}
-	socket.emit('message', data);
-	$('#message').val('');
-};
