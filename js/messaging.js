66a4d8f2e0654fd2badf92ff63b546c3fe0f4bc0
jim <jim@AthenasGhost>
2015-07-14 19:43:35 -0500

    changed all id's to reflect chit and chat

--- js/messaging.js
+++ js/messaging.js
@@ -2,6 +2,6 @@
 var socket = io();
 
 socket.on('message', function(data){
-	$('#walkie-view-chat').append($('<li>').text(data.user + ': ' + data.message));
+	$('#chat-feed').append($('<li>').text(data.user + ': ' + data.message));
 });
 
